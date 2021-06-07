66dce0267cff1216bf8bdb7cf471ec42e96c8954
Silasi baloyi <silassibaloy@gmail.com>
2021-06-07 10:14:16 +0200

    More bug fixes.

--- wiplayit_app/auth_backend/management/commands/connectextdb.py
+++ wiplayit_app/auth_backend/management/commands/connectextdb.py
@@ -14,7 +14,7 @@ class Command(BaseCommand):
     #    parser.add_argument('user_ids', nargs='+', type=int)
 
     def database_connect(self, *args, **kwargs):
-        DATABASE_NAME = os.getenv('DATABASE_NAME')
+        DATABASE_NAME = 'Baloyi$wiplayitdb'
         DATABASE_USER = os.getenv("DATABASE_USER")
         DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
         DATABASE_HOST = os.getenv('DATABASE_HOST')
@@ -32,43 +32,40 @@ class Command(BaseCommand):
         try:
             connection = self.database_connect()
             cursor = connection.cursor()
-            socialaccounts = "SELECT * FROM socialaccount_socialaccount"
+            
             socialaccount_apps = "SELECT * FROM socialaccount_socialapp"
             socialaccount_socialtoken = "SELECT * FROM socialaccount_socialtoken"
-
-
-            cursor.execute(socialaccounts)
-            columns = cursor.description
-            results = cursor.fetchall()
-
-            list_results = []
-            for k, row in enumerate(results):
-                dict_results = dict()
-
-                for i, value in enumerate(row):
-                    key = columns[i][0]
-                    dict_results[key] = value 
-
-                list_results.append(dict_results)
-
-            print(list_results)
+            self.extract_socialacconts(cursor)
+           
             cursor.connection.close()
         
         except Exception as e:
             raise e
 
         self.stdout.write(self.style.SUCCESS('Successfully'))
+
+
+    def extract_socialacconts(self, cursor):
+        socialaccounts = "SELECT * FROM socialaccount_socialaccount"
+
+        cursor.execute(socialaccounts)
+        columns = cursor.description
+        results = cursor.fetchall()
+
+        for k, row in enumerate(results):
+            dict_results = dict()
+
+            for i, value in enumerate(row):
+                key = columns[i][0]
+                dict_results[key] = value 
+
+            print(dict_results)
+
+    def extract_socialapps(self, cursor):
+        pass
+
+    def extract_socialtokens(self, cursor):
+        pass
        
 
-        #self.stdout.write('Database connection is {0}'.format(connection))
-        
-        '''
-        for user_id in options['user_ids']:
-            try:
-                user = User.objects.get(pk=user_id)
-            except User.DoesNotExist:
-                raise CommandError('User "%s" does not exist' % user_id)
-
-            
-            self.stdout.write(self.style.SUCCESS('Successfully got user "%s"' % user_id))
-        '''
+       
