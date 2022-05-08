41942490fcf23fcabe4b5f33baf4f8654fd20286
silasi <silassibaloy@gmail.com>
2022-05-08 15:57:01 +0200

    More bug fixing.

--- main_app/management/commands/databackup.py
+++ main_app/management/commands/databackup.py
@@ -1,8 +1,10 @@
 import os
 import json
+from sqlite3 import connect
 from django.core.management.base import BaseCommand, CommandError
 
 import MySQLdb as Database
+from MySQLdb._exceptions import ProgrammingError 
 from allauth.socialaccount.models import SocialAccount, SocialApp, SocialToken
 from allauth.account.models import EmailAddress
 from django.contrib.sites.models import Site
@@ -23,16 +25,31 @@ def upload_social_account_avatar(socialaccount=None):
     profile.profile_picture = avatar
     profile.save()
 
+    
 
 class Command(BaseCommand):
     help = 'Backup data from another database to another'
+    database_name = None
+    database_table = None
+    current_users = None
+    previous_users = None
+
 
     def add_arguments(self, parser):
         parser.add_argument('database_name', nargs='+', type=str)
-
-    def database_connect(self, *args, **kwargs):
+                 
+        parser.add_argument(
+            '--old_tables',
+            action='store_true',
+            help='Search old database table.',
+        )
         
-        DATABASE_NAME =  args[0]
+      
+    def database_connect(self, *args, **kwargs):
+    
+        self.database_name = args[0]
+        
+        DATABASE_NAME =  self.database_name
         DATABASE_USER = os.getenv("DATABASE_USER")
         DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
         DATABASE_HOST = os.getenv('DATABASE_HOST')
@@ -44,9 +61,9 @@ class Command(BaseCommand):
             passwd=DATABASE_PASSWORD,
             database=DATABASE_NAME
         )
-        
 
     def handle(self, *args, **options):
+                    
         for database in options['database_name']:
             try:
                 connection = self.database_connect(database)
@@ -59,6 +76,7 @@ class Command(BaseCommand):
             self.extract_users(cursor)
             self.extract_socialapps(cursor)
             self.extract_socialaccounts(cursor)
+            
             self.extract_socialtokens(cursor)
             self.extract_email_address(cursor)
             self.extract_phone_numbers(cursor)
@@ -66,20 +84,37 @@ class Command(BaseCommand):
             
             self.extract_questions(cursor)
             self.extract_answers(cursor)
-            #self.extract_comments(cursor, 'comments')
-            #self.extract_replies(cursor, 'replies')
-            #self.extract_comments(cursor, 'post_comments')
-            #self.extract_replies(cursor, 'answer_replies')
+
+            if options['old_tables']:
+                self.extract_comments(cursor, 'answer_comments')
+                self.extract_replies(cursor, 'post_replies')
+                self.extract_comments(cursor, 'post_comments')
+                self.extract_replies(cursor, 'answer_replies')
+            else:
+                self.extract_comments(cursor, 'comments')
+                self.extract_replies(cursor, 'replies')
+                        
       
             cursor.connection.close()
                   
             self.stdout.write(self.style.SUCCESS('Successfully'))
 
-    def extract_posts(self, cursor):
-        posts = "SELECT * FROM posts"
-        cursor.execute(posts)
-        columns = cursor.description
-        results = cursor.fetchall()
+
+    def get_query(self, cursor, query):
+        try:
+            cursor.execute(query)
+            columns = cursor.description
+            results = cursor.fetchall()
+            return (results, columns) 
+
+        except:
+            raise ProgrammingError(
+                "Table {0}.{1} does not exist".format(self.database_name, self.database_table)
+            )
+
+    def serialize_data_query(self, cursor, query):
+        data = []
+        results, columns = self.get_query(cursor, query)
 
         for k, row in enumerate(results):
             dict_results = dict()
@@ -89,239 +124,291 @@ class Command(BaseCommand):
                 if key == 'add_post':
                     key = 'post'
 
-                if key == 'add_title':
+                elif key == 'add_title':
                     key = 'title'
 
-                dict_results[key] = value 
+                elif key == 'add_question':
+                    key = 'question'
 
-            self.save(Post, dict_results)
+                elif key == 'primary_number':
+                    key = 'phone_number'
+
+                dict_results[key] = value
+            
+            data.append(dict_results)
+        
+        return data
+
+  
+    def data_exist(self, model, data) -> bool:
+        return model.objects.filter(**data).exists()
+
+    def get_user(self, id):
+        users = self.previous_users
+        user = None
+
+        for u in users:
+            if u and u['id'] == id:
+                search_data = {
+                    "email":u['email'],
+                    "first_name":u['first_name'],
+                    "last_name":u['last_name'],
+                    
+                    }
+                
+                if  self.data_exist(User, search_data):
+                    user = User.objects.get(**search_data)
+                    return user
+        return user
+
+
+    def extract_posts(self, cursor):
+        self.database_table = 'posts'
+        query = "SELECT * FROM posts"
+        posts = self.serialize_data_query(cursor, query)
+        
+        for post in posts:
+            user = self.get_user(post['author_id'])
+            if user:
+                post['author_id'] = user.id
+
+            search_data = {
+                "post":post['post'],
+                "slug":post['slug'],
+                "author_id":post['author_id']
+            }
+
+            if not self.data_exist(Post, search_data):
+                self.save(Post,  post)
+
            
 
     def extract_questions(self, cursor):
-        questions = "SELECT * FROM questions"
-        cursor.execute(questions)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM questions"
+        self.database_table = 'questions'
+        questions = self.serialize_data_query(cursor, query)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        for question in questions:
+            user = self.get_user(question['author_id'])
+            if user:
+                question['author_id'] = user.id
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
+            search_data = {
+                "question":question['question'],
+                "slug":question['slug'],
+                "author_id":question['author_id']
+            }
+            if not self.data_exist(Question, search_data):
+                self.save(Question, question)
 
-                if key == 'add_question':
-                    key = 'question'
-
-                dict_results[key] = value 
-
-            self.save(Question, dict_results)
-
-
+    
     def extract_answers(self, cursor):
-        answers = "SELECT * FROM answers"
-        cursor.execute(answers)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM answers"
+        self.database_table = 'answers'
+    
+        answers = self.serialize_data_query(cursor, query) 
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        for answer in answers:
+            user = self.get_user(answer['author_id'])
+            if user:
+                answer['author_id'] = user.id
+            
+            search_data = {
+                "answer":answer['answer'],
+                "author_id":answer['author_id']
+            }
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                if key == 'add_answer':
-                    key = 'answer'
-                dict_results[key] = value 
-            self.save(Answer, dict_results)
+            if not self.data_exist(Answer, search_data):
+                self.save(Answer, answer)
             
 
-    def extract_comments(self, cursor, table_name=None):
-        if table_name == None:
-            table_name = 'comments'
-        
-        comments = "SELECT * FROM {0}".format(table_name)
-        cursor.execute(comments)
-        columns = cursor.description
-        results = cursor.fetchall()
+    def extract_comments(self, cursor, table_name):
+             
+        query = "SELECT * FROM {0}".format(table_name)
+        self.database_table = table_name
+        comments = self.serialize_data_query(cursor, query)
+        #print(comments)
+        for comment in comments:
+            user = self.get_user(comment['author_id'])
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+            if user:
+                comment['author_id'] = user.id
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
-            self.save(Comment, dict_results)
+            search_data = {
+                "comment":comment['comment'],
+                "author_id":comment['author_id']
+            }
 
+            if not self.data_exist(Comment, search_data):
+                self.save(Comment, comment)
+    
         
     def extract_replies(self, cursor, table_name=None):
         if table_name == None:
             table_name = 'replies'
 
-        replies = "SELECT * FROM {0}".format(table_name)
-        cursor.execute(replies)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM {0}".format(table_name)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        self.database_table = table_name
+        replies = self.serialize_data_query(cursor, query)
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
+        for reply in replies:
+            user = self.get_user(reply['author_id'])
+            
+            if user:
+                reply['author_id'] = user.id
 
-            self.save(Reply, dict_results)
+            search_data = {"reply":reply['reply'], "author_id":reply['author_id']}
+            
+            if not self.data_exist(Reply, search_data):
+                #self.save(Reply, reply)
+                pass
  
 
 
     def extract_users(self, cursor):
-        users = "SELECT * FROM users"
-        cursor.execute(users)
-        columns = cursor.description
-        results = cursor.fetchall()
+                
+        self.database_table = 'users'
+        query = "SELECT * FROM users"
+        users = self.serialize_data_query(cursor, query)
+        self.previous_users = users
+        
+        for user in users:
+            email = user['email']
+            search_data = {"email":email}
+            if not self.data_exist(User, search_data):
+                self.save(User, user)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
-
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
-
-            self.save(User, dict_results)
-
-           
+                   
     def extract_socialaccounts(self, cursor):
         query = "SELECT * FROM socialaccount_socialaccount"
+        self.database_table = 'users'
+        social_accounts = self.serialize_data_query(cursor, query)
+        
+        for account in social_accounts:
 
-        cursor.execute(query)
-        columns = cursor.description
-        results = cursor.fetchall()
-               
-        for k, row in enumerate(results):
-            dict_results = dict()
-            
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
-                                    
-            extra_data = dict_results['extra_data']
+            extra_data = account['extra_data']
             extra_data = json.loads(extra_data)
+            
 
-            user=dict_results.get('user_id', None)
-            uid=dict_results.get('uid', None)
-            provider=dict_results.get('provider', None)
-            print(dict_results)
+            uid = account['uid']
+            user_id = account['user_id']
+            provider = account['provider']
+            user = self.get_user(user_id)
+            if user:
+                account['user_id'] = user.id
+                user_id = user.id
 
-            socialaccounts = SocialAccount.objects.filter(user=user, uid=uid)
-            socialaccount = None
-           
-            print(socialaccounts)
+            search_data = {"uid":uid, "user_id":user_id, "provider":provider}
 
-            if socialaccounts:
-                print('Updating existing socia user')
-                socialaccount = socialaccounts[0]
-                socialaccount.extra_data = extra_data
-                socialaccount.save()
+            if not self.data_exist(SocialAccount, search_data):
+                account['extra_data'] = extra_data
+                account = self.save(SocialAccount, account)
 
-            elif dict_results:
-                print('Creating new social user')
-                dict_results['extra_data'] = extra_data
-                socialaccount = self.save(SocialAccount, dict_results)  
-
-            print(socialaccount)
-            print('')
-            upload_social_account_avatar(socialaccount)          
+                upload_social_account_avatar(account) 
+                
+              
                   
     def extract_socialapps(self, cursor):
-        socialaccount_apps = "SELECT * FROM socialaccount_socialapp"
-        cursor.execute(socialaccount_apps)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM socialaccount_socialapp"
+        self.database_table = 'socialaccount_socialapp'
+        social_apps = self.serialize_data_query(cursor, query)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        for app in social_apps:
+            search_data = {
+                "provider":app['provider'],
+                "name":app['name'],
+                "client_id":app['client_id'],
+                "secret":app['secret']
+            }
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
-
-            self.save(SocialApp, dict_results)
+            if not self.data_exist(SocialApp, search_data):
+                self.save(SocialApp, app)
 
         
 
     def extract_socialtokens(self, cursor):
-        socialaccount_socialtoken = "SELECT * FROM socialaccount_socialtoken"
-        cursor.execute(socialaccount_socialtoken)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM socialaccount_socialtoken"
+        self.database_table = 'socialaccount_socialtoken'
+        social_tokens = self.serialize_data_query(cursor, query)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        for token in social_tokens:
+            search_data = {
+                "app":token['app'],
+                "account":token['account'],
+                "token":token['token'],
+                "token_secret":token['token_secret']
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
-            
-            self.save(SocialToken, dict_results)
+            }
+            if not self.data_exist(SocialToken, search_data):
+                self.save(SocialToken, token)
          
     def extract_email_address(self, cursor):
-        email_address = "SELECT * FROM account_emailaddress"
-        cursor.execute(email_address)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM account_emailaddress"
+        self.database_table = 'account_emailaddress'
+        email_address = self.serialize_data_query(cursor, query)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        for email in email_address:
+            user = self.get_user(email['user_id'])
+            if user:
+                email['user_id'] = user.id
+                
+            search_data = {
+                "email":email['email'],
+                "user_id":email['user_id'],
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value
-
-            self.save(EmailAddress, dict_results)
+            }
+            if not self.data_exist(EmailAddress, search_data):
+                self.save(EmailAddress, email)
 
     def extract_sites(self, cursor):
-        sites = "SELECT * FROM django_site"
-        cursor.execute(sites)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM django_site"
+        self.database_table = 'django_site'
+        sites = self.serialize_data_query(cursor, query)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
-
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value
-
-            self.save(Site, dict_results)
+        for site in sites:
+            search_data = {
+                "domain":site['domain'],
+                "name":site['name']
+            }
+            if not self.data_exist(Site, search_data):
+                self.save(Site, site)
 
 
            
     def extract_phone_numbers(self, cursor):
-        email_address = "SELECT * FROM users_phone_numbers"
-        cursor.execute(email_address)
-        columns = cursor.description
-        results = cursor.fetchall()
+        query = "SELECT * FROM users_phone_numbers"
+        self.database_table = 'users_phone_numbers'
+        phone_numbers = self.serialize_data_query(cursor, query)
 
-        for k, row in enumerate(results):
-            dict_results = dict()
+        for phone_number in phone_numbers:
+            user = self.get_user(phone_number['user_id'])
+            if user:
+                phone_number['user_id'] = user.id
 
-            for i, value in enumerate(row):
-                key = columns[i][0]
-                dict_results[key] = value 
+            search_data = {
+                "phone_number":phone_number['phone_number'],
+                "user_id":phone_number['user_id']
+            }
 
-            self.save(PhoneNumber, dict_results)
+            if not self.data_exist(PhoneNumber, search_data):
+                self.save(PhoneNumber, phone_number)
+        
           
 
     def save(self, model, data):
         data.pop('id', False)
-        data_query = model.objects.filter(**data)
         
-        if len(data_query) == 0:
+        try:
             data, created = model.objects.get_or_create(**data)
             if created:
                 return data
-            return None
-        else:
-            return data_query[0]
-            
-              
+            else:
+                return None
+                
+        except Exception as error:
+            raise ProgrammingError(error)
 
-       
+        
+            
+                     
